ff9b8ecfe9d8df2a60c7dce8bc860a9527ff3932
konekotorre <76505505+konekotorre@users.noreply.github.com>
2021-01-31 22:45:06 -0500

    commit 33

--- app/Exports/InfoFinGenExport.php
+++ app/Exports/InfoFinGenExport.php
@@ -13,6 +13,7 @@ class InfoFinGenExport implements FromCollection, WithHeadings
         $contacto_busqueda = DB::table('informacion_financieras')
             ->leftJoin('organizacions', 'organizacions.id', '=', 'informacion_financieras.organizacion_id')
             ->leftJoin('tipo_documento_organizacions', 'tipo_documento_organizacions.id', '=', 'organizacions.tipo_documento_organizacion_id')
+            ->leftJoin('subsectors', 'subsectors.id', '=', 'organizacions.subsector_id')
             ->leftJoin('clasificacions', 'clasificacions.id', '=', 'informacion_financieras.clasificacion_id')
             ->leftJoin('regimens', 'regimens.id', '=', 'informacion_financieras.regimen_id')
             ->leftJoin('users', 'users.id', '=', 'informacion_financieras.usuario_actualizacion')
