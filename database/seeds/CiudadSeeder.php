512adf9fe694f950c01b0d45b766f01e0429d080
konekotorre <76505505+konekotorre@users.noreply.github.com>
2021-04-15 01:32:28 -0500

    commit 53

--- database/seeds/CiudadSeeder.php
+++ database/seeds/CiudadSeeder.php
@@ -16,413 +16,413 @@ class CiudadSeeder extends Seeder
 
         DB::table('ciudads')->insert([
             'nombre' => 'Leticia',
-            'departamento_estado_' => '1'
+            'departamento_estado_id' => '1'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Medellin',
-            'departamento_estado_' => '2'
+            'departamento_estado_id' => '2'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Arauca',
-            'departamento_estado_' => '3'
+            'departamento_estado_id' => '3'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Barranquilla',
-            'departamento_estado_' => '4'
+            'departamento_estado_id' => '4'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Cartagena',
-            'departamento_estado_' => '5'
+            'departamento_estado_id' => '5'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Tunja',
-            'departamento_estado_' => '6'
+            'departamento_estado_id' => '6'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Manizales',
-            'departamento_estado_' => '7'
+            'departamento_estado_id' => '7'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Florencia',
-            'departamento_estado_' => '8'
+            'departamento_estado_id' => '8'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Yopal',
-            'departamento_estado_' => '9'
+            'departamento_estado_id' => '9'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Popayán',
-            'departamento_estado_' => '10'
+            'departamento_estado_id' => '10'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Valledupar',
-            'departamento_estado_' => '11'
+            'departamento_estado_id' => '11'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Quindó',
-            'departamento_estado_' => '12'
+            'departamento_estado_id' => '12'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Monteria',
-            'departamento_estado_' => '13'
+            'departamento_estado_id' => '13'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Bogotá',
-            'departamento_estado_' => '14'
+            'departamento_estado_id' => '14'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Iníra',
-            'departamento_estado_' => '15'
+            'departamento_estado_id' => '15'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'San José del Guaviare',
-            'departamento_estado_' => '16'
+            'departamento_estado_id' => '16'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Neiva',
-            'departamento_estado_' => '17'
+            'departamento_estado_id' => '17'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Rioacha',
-            'departamento_estado_' => '18'
+            'departamento_estado_id' => '18'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Santamarta',
-            'departamento_estado_' => '19'
+            'departamento_estado_id' => '19'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Villavicencio',
-            'departamento_estado_' => '20'
+            'departamento_estado_id' => '20'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Pasto',
-            'departamento_estado_' => '21'
+            'departamento_estado_id' => '21'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Cúcuta',
-            'departamento_estado_' => '22'
+            'departamento_estado_id' => '22'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Mocoa',
-            'departamento_estado_' => '23'
+            'departamento_estado_id' => '23'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Armenia',
-            'departamento_estado_' => '24'
+            'departamento_estado_id' => '24'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Pereira',
-            'departamento_estado_' => '25'
+            'departamento_estado_id' => '25'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'San Andrés',
-            'departamento_estado_' => '26'
+            'departamento_estado_id' => '26'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Bucaramanga',
-            'departamento_estado_' => '27'
+            'departamento_estado_id' => '27'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Sincelejo',
-            'departamento_estado_' => '28'
+            'departamento_estado_id' => '28'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Ibagué',
-            'departamento_estado_' => '29'
+            'departamento_estado_id' => '29'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Cali',
-            'departamento_estado_' => '30'
+            'departamento_estado_id' => '30'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Mitú',
-            'departamento_estado_' => '31'
+            'departamento_estado_id' => '31'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Puerto Carreño',
-            'departamento_estado_' => '32'
+            'departamento_estado_id' => '32'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Montgomery',
-            'departamento_estado_' => '33'
+            'departamento_estado_id' => '33'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Juneau',
-            'departamento_estado_' => '34'
+            'departamento_estado_id' => '34'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Phoenix',
-            'departamento_estado_' => '35'
+            'departamento_estado_id' => '35'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Little Rock',
-            'departamento_estado_' => '36'
+            'departamento_estado_id' => '36'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Sacramento',
-            'departamento_estado_' => '37'
+            'departamento_estado_id' => '37'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Raleigh',
-            'departamento_estado_' => '38'
+            'departamento_estado_id' => '38'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Columbia',
-            'departamento_estado_' => '39'
+            'departamento_estado_id' => '39'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Denver',
-            'departamento_estado_' => '40'
+            'departamento_estado_id' => '40'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Hartford',
-            'departamento_estado_' => '41'
+            'departamento_estado_id' => '41'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Bismarck',
-            'departamento_estado_' => '42'
+            'departamento_estado_id' => '42'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Pierre',
-            'departamento_estado_' => '43'
+            'departamento_estado_id' => '43'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Dover',
-            'departamento_estado_' => '44'
+            'departamento_estado_id' => '44'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Tallahassee',
-            'departamento_estado_' => '45'
+            'departamento_estado_id' => '45'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Atlanta',
-            'departamento_estado_' => '46'
+            'departamento_estado_id' => '46'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Honolulu',
-            'departamento_estado_' => '47'
+            'departamento_estado_id' => '47'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Boise',
-            'departamento_estado_' => '48'
+            'departamento_estado_id' => '48'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Springfield',
-            'departamento_estado_' => '49'
+            'departamento_estado_id' => '49'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Indianápolis',
-            'departamento_estado_' => '50'
+            'departamento_estado_id' => '50'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Des Moines',
-            'departamento_estado_' => '51'
+            'departamento_estado_id' => '51'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Topeka',
-            'departamento_estado_' => '52'
+            'departamento_estado_id' => '52'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Frankfort',
-            'departamento_estado_' => '53'
+            'departamento_estado_id' => '53'
         ]);
 
 
         DB::table('ciudads')->insert([
             'nombre' => 'Baton Rouge',
-            'departamento_estado_' => '54'
+            'departamento_estado_id' => '54'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Augusta',
-            'departamento_estado_' => '55'
+            'departamento_estado_id' => '55'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Annapolis',
-            'departamento_estado_' => '56'
+            'departamento_estado_id' => '56'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Boston',
-            'departamento_estado_' => '57'
+            'departamento_estado_id' => '57'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Lansing',
-            'departamento_estado_' => '58'
+            'departamento_estado_id' => '58'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Saint Paul',
-            'departamento_estado_' => '59'
+            'departamento_estado_id' => '59'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Jackson',
-            'departamento_estado_' => '60'
+            'departamento_estado_id' => '60'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Jefferson City',
-            'departamento_estado_' => '61'
+            'departamento_estado_id' => '61'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Helena',
-            'departamento_estado_' => '62'
+            'departamento_estado_id' => '62'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Lincoln',
-            'departamento_estado_' => '63'
+            'departamento_estado_id' => '63'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Carson City',
-            'departamento_estado_' => '64'
+            'departamento_estado_id' => '64'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Trenton',
-            'departamento_estado_' => '65'
+            'departamento_estado_id' => '65'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Albany',
-            'departamento_estado_' => '66'
+            'departamento_estado_id' => '66'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Concord',
-            'departamento_estado_' => '67'
+            'departamento_estado_id' => '67'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Santa Fe',
-            'departamento_estado_' => '68'
+            'departamento_estado_id' => '68'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Columbus',
-            'departamento_estado_' => '69'
+            'departamento_estado_id' => '69'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Oklahoma City',
-            'departamento_estado_' => '70'
+            'departamento_estado_id' => '70'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Salem',
-            'departamento_estado_' => '71'
+            'departamento_estado_id' => '71'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Harrisburg',
-            'departamento_estado_' => '72'
+            'departamento_estado_id' => '72'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Provence',
-            'departamento_estado_' => '73'
+            'departamento_estado_id' => '73'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Nashville',
-            'departamento_estado_' => '74'
+            'departamento_estado_id' => '74'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Austin',
-            'departamento_estado_' => '75'
+            'departamento_estado_id' => '75'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Salt Lake City',
-            'departamento_estado_' => '76'
+            'departamento_estado_id' => '76'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Montpelier',
-            'departamento_estado_' => '77'
+            'departamento_estado_id' => '77'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Richmond',
-            'departamento_estado_' => '78'
+            'departamento_estado_id' => '78'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Charleston',
-            'departamento_estado_' => '79'
+            'departamento_estado_id' => '79'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Olympia',
-            'departamento_estado_' => '80'
+            'departamento_estado_id' => '80'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Madison',
-            'departamento_estado_' => '81'
+            'departamento_estado_id' => '81'
         ]);
 
         DB::table('ciudads')->insert([
             'nombre' => 'Cheyenne',
-            'departamento_estado_' => '82'
+            'departamento_estado_id' => '82'
         ]);
 
 
