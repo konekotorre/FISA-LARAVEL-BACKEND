57ba4293669e2f19c8ba63152cbb3fdcf2942a6e
JoseSantamaria <jose.santamaria@segurosbolivar.com>
2021-06-19 21:15:10 -0500

    Commit fecha prueba

--- app/Http/Controllers/VisitaController.php
+++ app/Http/Controllers/VisitaController.php
@@ -111,8 +111,8 @@ class VisitaController extends Controller
 
     public function today()
     {
-        $now = Carbon::now()->format('d/m/Y');
-        $future = Carbon::now()->addDays(7)->format('d/m/Y');
+        $now = Carbon::now()->format('d-m-Y');
+        $future = Carbon::now()->addDays(7)->format('d-m-Y');
         $visitas = DB::table('visitas')
             ->join('organizacions', 'organizacions.id', '=', 'visitas.organizacion_id')
             ->join('estado_visitas', 'estado_visitas.id', '=', 'visitas.estado_id')
