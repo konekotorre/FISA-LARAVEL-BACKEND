ad47fd9fdf7ee7af7263ecf336dd07bfc7db3eb1
JoseSantamaria <jose.santamaria@segurosbolivar.com>
2021-06-28 19:35:05 -0500

    commit F17

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,25 +13,26 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
 
-            TipoDocumentoPersonaSeeder::class,
+/*          TipoDocumentoPersonaSeeder::class,
             UserSeeder::class,
             PermissionsSeeder::class,
-             PaisSeeder::class,
-             DepartamentoEstadoSeeder::class,
-             CiudadSeeder::class,
-             CiiuSeeder::class,
-             CategoriaSeeder::class,
-             ClaseSeeder::class,
-             RegimenSeeder::class,
-             SectorSeeder::class,
-             SubcategoriaSeeder::class,
-             SubsectorSeeder::class,
-             TipoOrganizacionSeeder::class,
-             TipoDocumentoOrganizacionSeeder::class,
-             TipoOficinaSeeder::class,
-             ClasificacionSeeder::class,
-             EstadoVisitaSeeder::class,
-             EstadoTareaSeeder::class,
+            PaisSeeder::class,
+            DepartamentoEstadoSeeder::class,
+            CiudadSeeder::class,
+            CiiuSeeder::class,
+            CategoriaSeeder::class,
+            ClaseSeeder::class,
+            RegimenSeeder::class,
+            SectorSeeder::class,
+            SubcategoriaSeeder::class,
+            SubsectorSeeder::class,
+            TipoOrganizacionSeeder::class,
+            TipoDocumentoOrganizacionSeeder::class,
+            TipoOficinaSeeder::class,
+            ClasificacionSeeder::class,
+            EstadoVisitaSeeder::class,
+            EstadoTareaSeeder::class, */
+            NewRoleSeeder::class,
         ]);
     }
 }
