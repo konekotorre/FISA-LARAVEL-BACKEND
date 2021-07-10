0feca022899ff66b8983c8303d85a07e882c5a17
konekotorre <76505505+konekotorre@users.noreply.github.com>
2021-07-10 14:45:56 -0500

    commit U5

---
+++ database/seeds/NewSeeder-10-07-2021.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class NewSeeder-10-07-2021 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
