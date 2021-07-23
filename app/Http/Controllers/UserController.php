1234fd63c985ccd74c8087d3b7ca35c8937e6f59
JoseSantamaria <jose.santamaria@segurosbolivar.com>
2021-07-22 21:41:57 -0500

    correccion editar usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\TipoDocumentoPersona;
 use App\User;
 
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\ModelHasRoles;
+use Spatie\Permission\Models\ModelHasRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -144,7 +144,7 @@ class UserController extends Controller
             $solicitud['password'] = bcrypt($solicitud['password']);
         }
         $user->update($solicitud);
-        ModelHasRoles::findOrFail($request->id)->delete();
+        ModelHasRole::where('role_id',$request->rol)->delete();
         if ($request->rol == 2) {
             $role = Role::find(2);
             $user->assignRole($role);
