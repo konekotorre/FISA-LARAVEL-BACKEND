73e9d8a55abf89fe890e143ca03cd5d5a98a3c5b
JoseSantamaria <jose.santamaria@segurosbolivar.com>
2021-10-13 21:17:43 -0500

    C02

--- app/Exports/OrgGenExport.php
+++ app/Exports/OrgGenExport.php
@@ -156,7 +156,9 @@ class OrgGenExport implements FromCollection, WithHeadings, WithStyles, WithColu
             $organizacion_busqueda[$i]->fecha_constitucion = $constitucion ? $constitucion->format('d/m/Y') : '';
             $created_at = new DateTime($organizacion_busqueda[$i]->created_at);
             $updated_at = new DateTime($organizacion_busqueda[$i]->updated_at);
-            $organizacion_busqueda[$i]->fecha_pauta = $fecha_pauta ? getdate($fecha_pauta): '';
+
+            $organizacion_busqueda[$i]->fecha_pauta = $$organizacion_busqueda[$i]->fecha_pauta ? date('d/m/Y', $fecha_pauta): '';
+            
             $organizacion_busqueda[$i]->fecha_afiliacion = $afiliacion ? $afiliacion->format('d/m/Y') : '';
             $organizacion_busqueda[$i]->fecha_desafiliacion = $desafiliacion ? $desafiliacion->format('d/m/Y') : '';
             $organizacion_busqueda[$i]->created_at = $created_at->format('d/m/Y');
