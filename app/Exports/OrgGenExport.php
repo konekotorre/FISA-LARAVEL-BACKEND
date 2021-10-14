e8201dc0ee4c39c9521f5d725b9ecd1feb90e551
konekotorre <konekotorre@gmail.com>
2021-10-14 04:57:53 -0500

    C04

--- app/Exports/OrgGenExport.php
+++ app/Exports/OrgGenExport.php
@@ -161,8 +161,8 @@ class OrgGenExport implements FromCollection, WithHeadings, WithStyles, WithColu
             $organizacion_busqueda[$i]->fecha_desafiliacion = $desafiliacion ? $desafiliacion->format('d/m/Y') : '';
             $organizacion_busqueda[$i]->created_at = $created_at->format('d/m/Y');
             $organizacion_busqueda[$i]->updated_at = $updated_at->format('d/m/Y'); */
-            $fecha_afiliacion = $organizacion_busqueda[$i]->fecha_afiliacion ? Date::dateTimeToExcel(new DateTime($organizacion_busqueda[$i]->fecha_afiliacion)): '';
-            $organizacion_busqueda[$i]->fecha_afiliacion = $fecha_afiliacion->format('d/m/Y');
+            $organizacion_busqueda[$i]->fecha_afiliacion = $organizacion_busqueda[$i]->fecha_afiliacion ? Date::dateTimeToExcel(new DateTime($organizacion_busqueda[$i]->fecha_afiliacion)): '';
+            
             $organizacion_busqueda[$i]->fecha_desafiliacion = $organizacion_busqueda[$i]->fecha_desafiliacion ? date('d/m/Y', strtotime($organizacion_busqueda[$i]->fecha_desafiliacion)): '';
             $organizacion_busqueda[$i]->fecha_constitucion = $organizacion_busqueda[$i]->fecha_constitucion ? date('d/m/Y', strtotime($organizacion_busqueda[$i]->fecha_constitucion)): '';
             $organizacion_busqueda[$i]->fecha_pauta = $organizacion_busqueda[$i]->fecha_pauta ? date('d/m/Y', strtotime($organizacion_busqueda[$i]->fecha_pauta)): '';
