fe4cc31d96f7844621df13f13316806b0e04e240
José Luis Santamaría <konekotorre@gmail.com>
2021-11-10 18:20:06 -0500

    fecha prueba 01

--- app/Http/Controllers/OrganizacionController.php
+++ app/Http/Controllers/OrganizacionController.php
@@ -208,7 +208,7 @@ class OrganizacionController extends Controller
             ->where('organizacions.id', '=', $organizacion->id)
             ->get();
 
-        $organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? new DateTime($organizacion_busqueda[0]->fecha_afiliacion):null;
+        //$organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? new DateTime($organizacion_busqueda[0]->fecha_afiliacion):null;
        // $organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? $organizacion_busqueda[0]->fecha_afiliacion->format('d/m/Y'):null;
 
         $creador_busqueda = DB::table('organizacions')
