342f7b815590adddef72969e712d54fe71bfd7fb
José Luis Santamaría <konekotorre@gmail.com>
2021-11-10 18:26:45 -0500

    fecha prueba 01

--- app/Http/Controllers/InformacionFinancieraController.php
+++ app/Http/Controllers/InformacionFinancieraController.php
@@ -9,6 +9,7 @@ use App\InformacionFinanciera;
 use App\Clasificacion;
 use App\Regimen;
 use App\Pais;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
