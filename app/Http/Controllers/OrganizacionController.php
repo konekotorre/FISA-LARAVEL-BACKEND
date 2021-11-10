24946493ae134f0bfb51d8e7a2650281dee0f392
José Luis Santamaría <konekotorre@gmail.com>
2021-11-10 18:21:13 -0500

    fecha prueba 01

--- app/Http/Controllers/OrganizacionController.php
+++ app/Http/Controllers/OrganizacionController.php
@@ -208,8 +208,8 @@ class OrganizacionController extends Controller
             ->where('organizacions.id', '=', $organizacion->id)
             ->get();
 
-        //$organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? new DateTime($organizacion_busqueda[0]->fecha_afiliacion):null;
-       // $organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? $organizacion_busqueda[0]->fecha_afiliacion->format('d/m/Y'):null;
+        $organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? new DateTime($organizacion_busqueda[0]->fecha_afiliacion):null;
+        $organizacion_busqueda[0]->fecha_afiliacion = $organizacion_busqueda[0]->fecha_afiliacion ? $organizacion_busqueda[0]->fecha_afiliacion->format('Y/m/d'):null;
 
         $creador_busqueda = DB::table('organizacions')
             ->leftJoin('users', 'users.id', '=', 'organizacions.usuario_creacion')
