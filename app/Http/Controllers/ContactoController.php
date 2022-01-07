4c6d7d6ed3132a184b03fac49671f8856332dee5
José Luis Santamaría <konekotorre@gmail.com>
2022-01-06 21:50:27 -0500

    BC42

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -146,14 +146,6 @@ class ContactoController extends Controller
                 'contactos.observaciones',
                 'organizacions.nombre as organizacion',
             )
-            // ->when($tercer_nombre, function ($query, $tercer_nombre) {
-            //     $query->where('personas.nombres', 'ilike', $tercer_nombre)
-            //         ->orWhere('personas.apellidos', 'ilike', $tercer_nombre);
-            // })
-            // ->when($cuarto_nombre, function ($query, $cuarto_nombre) {
-            //     $query->where('personas.nombres', 'ilike', $cuarto_nombre)
-            //         ->orWhere('personas.apellidos', 'ilike', $cuarto_nombre);
-            // })
             ->when($subcategorias, function ($query, $subcategorias) {
                 return $query->whereIn('detalle_categoria_personas.subcategoria_id', $subcategorias);
             })
@@ -184,15 +176,6 @@ class ContactoController extends Controller
             ->when($ciudad, function ($query, $ciudad) {
                 return  $query->where('oficinas.ciudad_id', $ciudad);
             })
-            // ->when($p_name, function ($query, $p_name) {
-            //     return $query->where('personas.nombres', 'ilike','%'.$p_name.'%');
-            // })
-            // ->when($p_name, function ($query, $p_name) {
-            //     return $query->where('personas.apellidos', 'ilike', '%'.$p_name.'%');
-            // })
-            // ->when($s_name, function ($query, $s_name) {
-            //     return $query->where('personas.nombres', 'ilike', '%'.$s_name.'%');
-            // })
             ->distinct('personas.id')
             ->orderBy('personas.id')
             ->get();
@@ -231,36 +214,6 @@ class ContactoController extends Controller
                         array_push($contactos_salida, $contactos[$i]);
                     }
                 }
-
-                //     if (strpos(strtolower($name), strtolower($p_name)) !== false) {
-                //         if (strpos(strtolower($name), strtolower(isset($s_name))) !== false) {
-
-                //             }
-                //             array_push($contactos_salida, $contactos[$i]);
-                //         }
-                //             array_push($contactos_salida, $contactos[$i]);
-                //         }
-
-                //          if ($s_name) {
-                //              if (strpos(strtolower($name), strtolower($s_name)) !== false) {
-                //                  if ($t_name) {
-                //                     if (strpos(strtolower($name), strtolower($t_name)) !== false) {
-                //                          if ($c_name) {
-                //                              if (strpos(strtolower($name), strtolower($c_name)) !== false) {
-                //                            } else {
-                //                                 array_push($contactos_salida, $contactos[$i]);
-                //                              }
-                //                          }
-                //                      } else {
-                //                          array_push($contactos_salida, $contactos[$i]);
-                //                     }
-                //                  }
-                //              } else {
-                //                  array_push($contactos_salida, $contactos[$i]);
-                //              }
-                //          }
-                //     }
-                // }
             }
         } else {
             $contactos_salida = $contactos;
