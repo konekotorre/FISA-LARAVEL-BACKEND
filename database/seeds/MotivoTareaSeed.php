30d3e54d2f49a80ccd49e1c5badc23a2fcfbf5ac
konekotorre <konekotorre@gmail.com>
2022-01-08 03:28:23 +0000

    nuevos seeds para motivos

---
+++ database/seeds/MotivoTareaSeed.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MotivoTareaSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
