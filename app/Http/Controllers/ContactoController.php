5589d56df2363ab7fc654f342a3ba9bd0c58f1eb
José Luis Santamaría <konekotorre@gmail.com>
2022-02-01 03:45:48 -0500

    cambio a buscador orgs sin repetir las mismas

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -218,7 +218,7 @@ class ContactoController extends Controller
         }
 
         $contactos_salida = collect($contactos_salida);
-        $cont_final = $contactos_salida->groupBy('personas.nombre')->first();
+        $cont_final = $contactos_salida->groupBy('nombre')->first();
 
         return response()->json([
             "success" => true,
