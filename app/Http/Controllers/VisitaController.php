cf227bcdf08cf100ae5481d274cc1306601e0172
José Luis Santamaría <konekotorre@gmail.com>
2022-02-23 03:48:57 -0500

    buscador visitas para pruebas

--- app/Http/Controllers/VisitaController.php
+++ app/Http/Controllers/VisitaController.php
@@ -30,9 +30,10 @@ class VisitaController extends Controller
                 'visitas.fecha_programada',
                 'motivo_visitas.nombre as motivo',
                 'estado_visitas.nombre as estado',
-                (DB::raw('count(tareas.id) as totalTareas')->groupBy('tareas.id'))
+                (DB::raw('count(tareas.id) as totalTareas'))
             )
             ->orderBy('visitas.fecha_programada')
+            ->groupBy('tareas.id')
             ->get();
         return response()->json([
             "success" => true,
