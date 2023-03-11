97c65feaff0987a07815ac9ae234df3727328b6a
JoseSantaSB <jose.santamaria@segurosbolivar.com>
2023-03-11 09:55:34 -0500

    test busqueda contactos

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -156,12 +156,12 @@ class ContactoController extends Controller
 
         $contactos = DB::table('contactos')
             ->join('personas', 'personas.id', 'contactos.persona_id')
-            ->join('oficinas', 'oficinas.id', 'contactos.oficina_id')
-            ->join('ciudads', 'ciudads.id', 'oficinas.ciudad_id')
-            ->join('departamento_estados', 'departamento_estados.id', 'oficinas.departamento_estado_id')
-            ->join('pais', 'pais.id', 'oficinas.pais_id')
-            ->join('organizacions', 'organizacions.id', 'contactos.organizacion_id')
-            ->join('detalle_categoria_personas', 'detalle_categoria_personas.persona_id', 'personas.id')
+            ->leftJoin('oficinas', 'oficinas.id', 'contactos.oficina_id')
+            ->leftJoin('ciudads', 'ciudads.id', 'oficinas.ciudad_id')
+            ->leftJoin('departamento_estados', 'departamento_estados.id', 'oficinas.departamento_estado_id')
+            ->leftJoin('pais', 'pais.id', 'oficinas.pais_id')
+            ->leftJoin('organizacions', 'organizacions.id', 'contactos.organizacion_id')
+            ->leftJoin('detalle_categoria_personas', 'detalle_categoria_personas.persona_id', 'personas.id')
             ->select(
                 'contactos.id as contacto_id',
                 'personas.id as persona_id',
@@ -206,7 +206,7 @@ class ContactoController extends Controller
             })
             ->orderBy('personas.id', 'ASC')
             ->orderBy($orderKey, $orderType)
-            ->distinct('personas.id')
+            ->distinct()
             ->get();
 
 
