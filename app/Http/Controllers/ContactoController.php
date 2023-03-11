117972e9b42cfbe17cd2b990290f8773e904fab0
JoseSantaSB <jose.santamaria@segurosbolivar.com>
2023-03-11 01:37:06 -0500

    Segundo intento paginado en consulta de contactos

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -161,8 +161,8 @@ class ContactoController extends Controller
             ->leftJoin('departamento_estados', 'departamento_estados.id', 'oficinas.departamento_estado_id')
             ->leftJoin('pais', 'pais.id', 'oficinas.pais_id')
             ->leftJoin('organizacions', 'organizacions.id', 'contactos.organizacion_id')
-            ->join('sectors', 'sectors.id', 'organizacions.sector_id')
-            ->join('subsectors', 'subsectors.id', 'organizacions.subsector_id')
+            ->leftJoin('sectors', 'sectors.id', 'organizacions.sector_id')
+            ->leftJoin('subsectors', 'subsectors.id', 'organizacions.subsector_id')
             ->leftJoin('detalle_categoria_personas', 'detalle_categoria_personas.persona_id', 'personas.id')
             ->select(
                 'contactos.id as contacto_id',
