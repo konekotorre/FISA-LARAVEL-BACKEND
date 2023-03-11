09083babd7deb3b8760097788e5d80bece14cba3
JoseSantaSB <jose.santamaria@segurosbolivar.com>
2023-03-11 01:32:56 -0500

    Segundo intento paginado en consulta de contactos

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -206,7 +206,7 @@ class ContactoController extends Controller
             ->when($ciudad, function ($query, $ciudad) {
                 return  $query->where('oficinas.ciudad_id', $ciudad);
             })
-            ->distinct('contactos.id')
+            ->distinct('personas.id')
             ->get();
 
           /*   if($orderType){
