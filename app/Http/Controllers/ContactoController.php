d88c6673a762d22cb92b8d3f176240f613b78f89
JoseSantaSB <jose.santamaria@segurosbolivar.com>
2023-03-10 23:30:34 -0500

    Segundo intento paginado en consulta de contactos

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -134,11 +134,11 @@ class ContactoController extends Controller
         $sector = $request->sector;
         $subsector = $request->subsector;
 
-        if($request->skip === 1){
+/*         if($request->skip === 1){
             $skip = 0;
         }
-
-        $skip = $request->skip ? $request->skip : null;
+ */
+        $skip = $request->skip ? intval($request->skip,10) : null;
         $limit = $request->limit ? $request->limit : null;
 
         $count = Contacto::where('id', '>', 0)->count();
