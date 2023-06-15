b4d8c4edaca90bfe8a73a321877ed8fc2b653e84
JoseSantaSB <jose.santamaria@segurosbolivar.com>
2023-06-15 13:56:25 -0500

    prueba de nit ilike y ajuste de busquda contacto nombres 7

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -210,7 +210,7 @@ class ContactoController extends Controller
             ->when($orderRequest, function ($query) use ($orderKey, $orderType) {
                 return  $query->orderBy($orderKey, $orderType);
             })
-            ->groupBy('contacto_id')
+            ->groupBy('contactos.id')
             ->get();
 
         //$contactos = $contactos->unique('contacto_id');
